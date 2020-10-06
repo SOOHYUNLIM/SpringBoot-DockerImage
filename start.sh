34b241d4371ab10cc207c60c210cf2add9722d42
AE200403 <AE200403>
2020-10-06 18:03:08 +0900

    스크립트 수정

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-REPOSITORY=/home/ec2-user/build/build/libs
+#REPOSITORY=/home/ec2-user/build/build/libs
+#
+#JAR_NAME=$(ls $REPOSITORY/ | tail -n 1)
+#
+#echo "JAR Name: $JAR_NAME"
+#
+#java -jar $REPOSITORY/$JAR_NAME
 
-JAR_NAME=$(ls $REPOSITORY/ | tail -n 1)
-
-echo "JAR Name: $JAR_NAME"
-
-java -jar $REPOSITORY/$JAR_NAME
+java -jar /home/ec2-user/build/build/libs/demo-0.0.1-SNAPSHOT.jar
