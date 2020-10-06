367888f6811c7f42cfd9c321a684e7dbcfa42dd3
SOOHYUNLIM <sh_7682@naver.com>
2020-10-07 01:02:26 +0900

    스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "> Start Spring Boot Service Deploy..."
 
-java -jar /home/ec2-user/build/build/libs/demo-0.0.1-SNAPSHOT.jar
+cd /home/ec2-user/build/build/libs
+
+java -jar demo-0.0.1-SNAPSHOT.jar
