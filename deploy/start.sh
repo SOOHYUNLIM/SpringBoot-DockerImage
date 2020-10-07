e40ea344e15aa9002950a24814449d3ab5efbd11
AE200403 <AE200403>
2020-10-07 15:08:41 +0900

    log 출력

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
+DEPLOY_PATH=/home/ec2-user/build
+BUILD_JAR=$(ls $DEPLOY_PATH/*.jar)
 
-TEST_JAR=$(ls *.jar)
-nohup java -jar $TEST_JAR &
-#
-#DEPLOY_PATH=/home/ec2-user/build
-#BUILD_JAR=$(ls $DEPLOY_PATH/*.jar)
-#
-#JAR_NAME=$(basename $BUILD_JAR)
-#echo "> build 파일명: $JAR_NAME"
-#
-#CURRENT_PID=$(pgrep -f $JAR_NAME)
-#echo "> 현재 실행중인 애플리케이션 pid 확인"
-#
-#if [ -z $CURRENT_PID ]
-#then
-#  echo "> 현재 구동중인 애플리케이션이 없습니다."
-#else
-#  echo "> $CURRENT_PID ...종료!"
-#  kill -15 $CURRENT_PID
-#  sleep 5
-#fi
-#
-#echo "> 새 어플리케이션 배포: $DEPLOY_PATH/$JAR_NAME"
-#nohup java -jar $DEPLOY_PATH/$JAR_NAME &
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-uesr/deploy.log
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-uesr/deploy.log
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없습니다." >> /home/ec2-uesr/deploy.log
+else
+  echo "> $CURRENT_PID ...종료!"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 어플리케이션 배포: $DEPLOY_PATH/$JAR_NAME" >> /home/ec2-uesr/deploy.log
+nohup java -jar $DEPLOY_PATH/$JAR_NAME >> /home/ec2-uesr/deploy.log &
