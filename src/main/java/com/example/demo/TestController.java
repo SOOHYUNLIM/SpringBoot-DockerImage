3dea74beee55802a34613d2064bf087324a4327b
AE200403 <AE200403>
2020-10-08 16:41:21 +0900

    profile 추가

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,10 +1,24 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@AllArgsConstructor
 public class TestController {
+
+    private Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
 
     @GetMapping("/hello")
     public String test() {
