94c98154ca571587a7eb26cc0e17b5fc704312ca
AE200403 <AE200403>
2020-10-13 13:16:29 +0900

    test

--- deploy/start.sh
+++ deploy/start.sh
@@ -7,9 +7,9 @@ BUILD_JAR=$(ls $DEPLOY_PATH/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME"
 
-echo "> 새 어플리케이션 배포: $DEPLOY_PATH/$JAR_NAME / profile=$IDLE_PROFILE"
-
 IDLE_PROFILE=$(find_idle_profile)
+
+echo "> 새 어플리케이션 배포: $DEPLOY_PATH/$JAR_NAME / profile=$IDLE_PROFILE"
 
 #STDOUT Error 해결 방법: > /dev/null 2> /dev/null < /dev/null 추가 필요
 nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $DEPLOY_PATH/$JAR_NAME > /dev/null 2> /dev/null < /dev/null &
